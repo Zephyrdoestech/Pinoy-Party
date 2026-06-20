3f0d475911762994d407c658558f5f4e4486b50f
Lance Chan <chanlance.school@gmail.com>
2026-06-20 22:43:17 +0800

    Wire player list into minigame loading

--- pinoy-party/autoload/SceneLoader.gd
+++ pinoy-party/autoload/SceneLoader.gd
@@ -1,11 +1,19 @@
 # autoload/SceneLoader.gd
 extends Node
 
-signal scene_loaded
-
-func go_to_minigame(minigame_id: String) -> void:
-	var path := "res://scenes/minigames/%s/%s.tscn" % [minigame_id, minigame_id]
+func go_to_minigame(minigame_id: String, players: Array[int]) -> void:
+	var path := "res://scenes/minigames/%s/%s.tscn" % [minigame_id, minigame_id.to_snake_case()]
 	get_tree().change_scene_to_file(path)
+	# Defer so the new scene's _ready() has already run before we call start_game()
+	call_deferred(&"_start_minigame_deferred", players)
+
+func _start_minigame_deferred(players: Array[int]) -> void:
+	await get_tree().process_frame
+	var minigame := get_tree().current_scene
+	if minigame is BaseMinigame:
+		minigame.start_game(players)
+	else:
+		push_error("SceneLoader: loaded scene is not a BaseMinigame, cannot start_game().")
 
 func return_to_board() -> void:
-	get_tree().change_scene_to_file("res://scenes/board/Board.tscn")
+	get_tree().change_scene_to_file("res://scenes/Game.tscn")
