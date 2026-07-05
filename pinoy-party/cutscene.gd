6eff911c2a5d2961c0b72430bf90217eeca3e1e5
ren <guinitairen@gmail.com>
2026-07-05 16:53:29 +0800

    added skip cutscene

--- pinoy-party/cutscene.gd
+++ pinoy-party/cutscene.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var skip_button: Button = $SkipButton
 
 func _ready() -> void:
 	# Ensure the animation plays automatically
@@ -9,6 +10,13 @@ func _ready() -> void:
 	
 	# Connect to the finished signal to transition to the main game
 	anim_player.animation_finished.connect(_on_animation_finished)
+	
+	if skip_button:
+		skip_button.pressed.connect(_on_skip_button_pressed)
+
+func _on_skip_button_pressed() -> void:
+	anim_player.stop()
+	_on_animation_finished("1st_Cutscene")
 
 func _on_animation_finished(anim_name: String) -> void:
 	if anim_name == "1st_Cutscene":
